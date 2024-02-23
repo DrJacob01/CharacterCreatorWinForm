81998bb1c76eb47363e18a4bac239b04663dfbf6
DrJacob01 <119816008+DrJacob01@users.noreply.github.com>
2024-02-23 10:39:50 +0000

    Add files via upload

---
+++ The Character Creator/The Character Creator/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace The_Character_Creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.StrengthTextBox = new System.Windows.Forms.TextBox();
+            this.SpeedTextBox = new System.Windows.Forms.TextBox();
+            this.WisdomTextBox = new System.Windows.Forms.TextBox();
+            this.PerspicuityTextBox = new System.Windows.Forms.TextBox();
+            this.StaminaTextBox = new System.Windows.Forms.TextBox();
+            this.FabricationTextBox = new System.Windows.Forms.TextBox();
+            this.CharacterNameTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Character Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Strength";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Speed";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Wisdom";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Perspicuity";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Stamina";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(33, 259);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Fabrication";
+            // 
+            // StrengthTextBox
+            // 
+            this.StrengthTextBox.Location = new System.Drawing.Point(97, 88);
+            this.StrengthTextBox.Name = "StrengthTextBox";
+            this.StrengthTextBox.Size = new System.Drawing.Size(141, 20);
+            this.StrengthTextBox.TabIndex = 7;
+            // 
+            // SpeedTextBox
+            // 
+            this.SpeedTextBox.Location = new System.Drawing.Point(97, 123);
+            this.SpeedTextBox.Name = "SpeedTextBox";
+            this.SpeedTextBox.Size = new System.Drawing.Size(141, 20);
+            this.SpeedTextBox.TabIndex = 8;
+            // 
+            // WisdomTextBox
+            // 
+            this.WisdomTextBox.Location = new System.Drawing.Point(97, 156);
+            this.WisdomTextBox.Name = "WisdomTextBox";
+            this.WisdomTextBox.Size = new System.Drawing.Size(141, 20);
+            this.WisdomTextBox.TabIndex = 9;
+            // 
+            // PerspicuityTextBox
+            // 
+            this.PerspicuityTextBox.Location = new System.Drawing.Point(97, 189);
+            this.PerspicuityTextBox.Name = "PerspicuityTextBox";
+            this.PerspicuityTextBox.Size = new System.Drawing.Size(141, 20);
+            this.PerspicuityTextBox.TabIndex = 10;
+            // 
+            // StaminaTextBox
+            // 
+            this.StaminaTextBox.Location = new System.Drawing.Point(97, 223);
+            this.StaminaTextBox.Name = "StaminaTextBox";
+            this.StaminaTextBox.Size = new System.Drawing.Size(141, 20);
+            this.StaminaTextBox.TabIndex = 11;
+            // 
+            // FabricationTextBox
+            // 
+            this.FabricationTextBox.Location = new System.Drawing.Point(98, 256);
+            this.FabricationTextBox.Name = "FabricationTextBox";
+            this.FabricationTextBox.Size = new System.Drawing.Size(141, 20);
+            this.FabricationTextBox.TabIndex = 12;
+            // 
+            // CharacterNameTextBox
+            // 
+            this.CharacterNameTextBox.Location = new System.Drawing.Point(36, 41);
+            this.CharacterNameTextBox.Name = "CharacterNameTextBox";
+            this.CharacterNameTextBox.Size = new System.Drawing.Size(203, 20);
+            this.CharacterNameTextBox.TabIndex = 13;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(291, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(459, 245);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.Location = new System.Drawing.Point(36, 297);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(202, 44);
+            this.CreateButton.TabIndex = 15;
+            this.CreateButton.Text = "Create";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            this.CreateButton.Click += new System.EventHandler(this.CreateButton_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(39, 354);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(120, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Skil Points Available: 12";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.CreateButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CharacterNameTextBox);
+            this.Controls.Add(this.FabricationTextBox);
+            this.Controls.Add(this.StaminaTextBox);
+            this.Controls.Add(this.PerspicuityTextBox);
+            this.Controls.Add(this.WisdomTextBox);
+            this.Controls.Add(this.SpeedTextBox);
+            this.Controls.Add(this.StrengthTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox StrengthTextBox;
+        private System.Windows.Forms.TextBox SpeedTextBox;
+        private System.Windows.Forms.TextBox WisdomTextBox;
+        private System.Windows.Forms.TextBox PerspicuityTextBox;
+        private System.Windows.Forms.TextBox StaminaTextBox;
+        private System.Windows.Forms.TextBox FabricationTextBox;
+        private System.Windows.Forms.TextBox CharacterNameTextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button CreateButton;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ The Character Creator/The Character Creator/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows.Forms;
+
+namespace The_Character_Creator
+{
+    public partial class Form1 : Form
+    {
+        private SoundPlayer player;
+
+        public Form1()
+        {
+            InitializeComponent();
+            player = new SoundPlayer("C:\\Users\\XXXXX\\source\\repos\\wise-mystical-tree.wav"); // Provide the path to your MP3 file
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            // Parse the values from the textboxes
+            int strength, speed, wisdom, perspicuity, stamina, fabrication;
+
+            if (int.TryParse(StrengthTextBox.Text, out strength) &&
+                int.TryParse(SpeedTextBox.Text, out speed) &&
+                int.TryParse(WisdomTextBox.Text, out wisdom) &&
+                int.TryParse(PerspicuityTextBox.Text, out perspicuity) &&
+                int.TryParse(StaminaTextBox.Text, out stamina) &&
+                int.TryParse(FabricationTextBox.Text, out fabrication))
+            {
+                // Check if the sum of the values is equal to 12
+                if (strength + speed + wisdom + perspicuity + stamina + fabrication == 12)
+                {
+                    // Get the character name from the CharacterNameTextBox
+                    string characterName = CharacterNameTextBox.Text;
+                    // Perform your desired action when the condition is met
+                    MessageBox.Show($"Welcome, {characterName}! Enjoy your wise and mystical journey!");
+                    player.Play();
+                }
+                else
+                {
+                    // Display an error message when the sum is not 12
+                    MessageBox.Show("The sum of values must be 12!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                // Display an error message if any of the textbox values are not valid integers
+                MessageBox.Show("Please enter valid integer values in all textboxes!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
